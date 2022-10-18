f8c21e6a4f2e4994577db7dc9a344bd2202fc06f
teticio <teticio@gmail.com>
2022-10-18 15:30:18 +0100

    fix for newer versions of python

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ define([
     'edit/js/editor',
     'notebook/js/outputarea',
     'codemirror/lib/codemirror',
-], function(
+], function (
     $,
     requirejs,
     contents_service,
@@ -30,11 +30,11 @@ define([
         prefix: '',
         prefix2: '',
     };
-    
+
     var Gotoerror = function (nb) {
         var gotoerror = this;
         this.events = nb.events;
-        
+
         this.element = $("<div id='nbextension-gotoerror'>").addClass("input_area");
         this.close_button = $("<i>").addClass("fa fa-window-close gotoerror-close-btn");
         this.element.append(this.close_button);
@@ -50,7 +50,7 @@ define([
                 $('.gotoerror-code').height($("#nbextension-gotoerror").height() - $(".gotoerror-filename").outerHeight(true));
             }
         });
-        
+
         // finally, add me to the page
         $("body").append(this.element);
     };
@@ -70,14 +70,14 @@ define([
         this.element.css('display', 'block');
         this.close_button.show();
         $("#notebook-container").css('margin-left', 0);
-        
+
         $(".gotoerror-filename").html('<a target="_blank" href="' + url + '">' + file_path.replace(/^.*[\\\/]/, '') + '</a>');
         // adjust height accordingly
         $('.gotoerror-code').height($("#nbextension-gotoerror").height() - $(".gotoerror-filename").outerHeight(true));
         var base_url = utils.get_body_data('baseUrl');
-        var config = new configmod.ConfigSection('edit', {base_url: base_url});
+        var config = new configmod.ConfigSection('edit', { base_url: base_url });
         config.load();
-        var common_config = new configmod.ConfigSection('common', {base_url: base_url});
+        var common_config = new configmod.ConfigSection('common', { base_url: base_url });
         common_config.load();
         var contents = new contents_service.Contents({
             base_url: base_url,
@@ -95,7 +95,7 @@ define([
         var that = this;
         this.events.on('file_loaded.Editor', function (evt, model) {
             if (line_number) {
-                that.editor.codemirror.setSelection({line: line_number - 1, ch: 0}, {line: line_number, ch: 0});
+                that.editor.codemirror.setSelection({ line: line_number - 1, ch: 0 }, { line: line_number, ch: 0 });
             }
         });
         this.events.on('file_load_failed.Editor', function (evt, model) {
@@ -115,31 +115,30 @@ define([
 
         // override
         outputarea.OutputArea.prototype.append_error = function (json) {
+            var ansi_re = /\x1b\[[^m]*m([^:]*):(\d*)/;
             var tb = json.traceback;
             if (tb !== undefined && tb.length > 0) {
                 var toinsert = this.create_output_area()
                 var subarea = this.create_output_subarea({}, "output_text", 'text/plain');
                 var len = tb.length;
                 var s
-                
-                for (var i=0; i<len; i++) {
+
+                for (var i = 0; i < len; i++) {
                     s = tb[i] + '\n';
-                    
+
                     // just in case anything goes wrong
                     try {
-                        var ansi_re = /^\x1b\[(.*?)([@-~])/g;
-                        var ansi_re2 = /(?<!^)\x1b\[(.*?)([@-~])/g;
-                        var start = ansi_re.exec(s);
-                        var end = ansi_re2.exec(s);
-                        
+                        var match_filename = ansi_re.exec(s);
+
                         // grab filename
-                        if (start && end) {
-                            var formated_filename = s.substring(start.index, end.index + end[0].length);
-                            var filename = s.substring(start.index + start[0].length, end.index).replace(/\\/g, '/');
+                        if (match_filename) {
+                            var filename = match_filename[1];
+                            var line_number = match_filename[2]
                             var root = options.prefix.replace(/\\/g, '/');
                             var root2 = options.prefix2.replace(/\\/g, '/');
                             var match = filename.search(root);
                             var match2 = filename.search(root2);
+
                             if (match < 0) {
                                 if (match2 >= 0) {
                                     root = root2;
@@ -153,65 +152,56 @@ define([
                                 var url = window.location.href.split('/');
                                 var url = url[0] + '//' + url[2] + ('/edit/' + file_path).replace('//', '/');
                                 var eol = s.search('\\n')
-                                var rest_of_line = utils.fixConsole(s.substring(end.index + end[0].length, eol));
                                 var line = $("<pre/>");
                                 var link = $("<span/>");
-                                link.html(utils.fixConsole(formated_filename));
-
-                                // extract line number
-                                var line_number_re = /^\x1b\[(.*?)([@-~])-*> +\d+/gm;
-                                var line_number = line_number_re.exec(s);
-                                if (line_number) {
-                                    var number_re = /\d+$/g;
-                                    line_number = number_re.exec(line_number[0])[0];
-                                }
-
+                                line.append(utils.fixConsole(s.substring(0, match_filename.index)));
+                                link.html(utils.fixConsole(s.substring(match_filename.index, match_filename.index + match_filename[0].length)));
                                 link.click(new Function("gotoerror.expand('" + file_path + "', " + line_number + ", '" + url + "')"));
                                 link.css('cursor', 'pointer');
-                                link.append(rest_of_line);
                                 line.append(link);
+                                line.append(utils.fixConsole(s.substring(match_filename.index + match_filename[0].length, eol)));
                                 subarea.append(line);
-                                s = s.substring(eol+1);
+                                s = s.substring(eol + 1);
                             }
                         }
-                    } catch(err) {
+                    } catch (err) {
                         console.error('nbextension gotoerror', 'unhandled error:', err);
                     }
-                    
+
                     // add the rest of the lines
                     subarea.append($("<pre/>").html(utils.fixConsole(s)));
                 }
-                
-                 // add stack overflow button
+
+                // add stack overflow button
                 try {
                     var search_text = escape(tb[tb.length - 1].replace(/\x1b\[(.*?)([@-~])/g, ''));
                     var stackoverflow_button = $("<button/>").addClass("gotoerror-stackoverflow-btn");
-                    stackoverflow_button.click(function() {
+                    stackoverflow_button.click(function () {
                         window.open('https://www.google.com/search?q=' + search_text + '+site:stackoverflow.com');
                     });
                     stackoverflow_button.text("Search Stack Overflow");
                     subarea.append($("<p/>").html('&nbsp'));
                     subarea.append(stackoverflow_button);
-                } catch(err) {
+                } catch (err) {
                     console.error('nbextension gotoerror', 'unhandled error:', err);
-                }                    
-                    
+                }
+
                 toinsert.append(subarea);
                 toinsert.addClass("output_error");
                 this._safe_append(toinsert);
             }
         };
     }
-    
-    function setup_gotoerror () {
+
+    function setup_gotoerror() {
         // lazy, hook it up to Jupyter.notebook as the handle on all the singletons
         gotoerror = new Gotoerror(Jupyter.notebook)
         return gotoerror;
     }
-    
+
     function load_ipython_extension() {
         apply_patches();
-        
+
         // add css
         var link = document.createElement("link");
         link.type = "text/css";
@@ -221,7 +211,7 @@ define([
 
         // setup things to run on loading config/notebook
         Jupyter.notebook.config.loaded
-            .then(function update_options_from_config () {
+            .then(function update_options_from_config() {
                 $.extend(true, options, Jupyter.notebook.config.data['gotoerror']);
             })
             .then(function () {
